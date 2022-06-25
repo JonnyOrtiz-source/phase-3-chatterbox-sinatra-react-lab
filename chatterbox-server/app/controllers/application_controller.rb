3d6338972b2d97ae46d449873e6dd5156f887e3c
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-06-25 11:13:39 -0400

    Completed assignment

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,28 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+  # GET /messages: returns an array of all messages as JSON, ordered by created_at in ascending order.
+  get "/messages" do
+    messages = Message.all.order(:created_at)
+    messages.to_json
+  end
+  # POST /messages: creates a new message with a body and username from params, and returns the newly created post as JSON.
+  post "/messages" do
+    message = Message.create(body: params[:body], username: params[:username])
+    puts message
+    message.to_json
+  end
+  # PATCH /messages/:id: updates the body of the message using params, and returns the updated message as JSON.
+  patch "/messages/:id" do
+    message = Message.find(params[:id])
+    message.update(body: params[:body], username: params[:username])
+    message.to_json
+  end
+  # DELETE /messages/:id: deletes the message from the database.
+  delete "/messages/:id" do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+  end
   
 end
